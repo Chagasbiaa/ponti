d5e4abc36c08385f9969fdbda53f397d6e65d670
chagasbiaa <beatriz.silva1519@etec.sp.gov.br>
2022-09-13 18:25:43 -0300

    tela de splash

--- app/src/main/java/com/example/blindspot/MainActivity.java
+++ app/src/main/java/com/example/blindspot/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.blindspot;
 
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        progressBar = findViewById(R.id.progressBar);
+        textView = findViewById(R.id.numero);
+
+        progressBar.setMax(100);
+        progressBar.setScaleY(3f)
+        ;
+
+        progressAnimation();
+    }
+
+    public void progressAnimation(){
+        Object context;
+        ProgressBarAnimation anim = new ProgressBarAnimation(this, progressBar, textView, 0f, 100f);;
+        anim.setDuration(6000);
+        progressBar.setAnimation(anim);
     }
 }
