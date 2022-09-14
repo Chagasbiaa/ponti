27c577afab7303577f5f800a833f8741af68dd78
chagasbiaa <beatriz.silva1519@etec.sp.gov.br>
2022-09-14 16:49:00 -0300

    botões entrar, criar e registrar

--- app/src/main/java/com/example/blindspot/Cadastro.java
+++ app/src/main/java/com/example/blindspot/Cadastro.java
@@ -2,13 +2,29 @@ package com.example.blindspot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Cadastro extends AppCompatActivity {
+
+     Button registro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        registro=findViewById(R.id.btnentrar3);
+
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(Cadastro.this, Menu.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/blindspot/login.java
+++ app/src/main/java/com/example/blindspot/login.java
@@ -2,13 +2,45 @@ package com.example.blindspot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login extends AppCompatActivity {
+
+    Button crie;
+    Button entrar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        crie=findViewById(R.id.btncrie);
+
+        crie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                 Intent intent = new Intent(login.this, Cadastro.class);
+                 startActivity(intent);
+            }
+        });
+
+        entrar=findViewById(R.id.btnentrar);
+
+        entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(login.this, Menu.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
